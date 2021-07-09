85d718c57204c61f8bd65a9fd5270d0d73445109
sukristiyo <49105692+sukristiyo@users.noreply.github.com>
2021-07-09 20:11:45 +0700

    Catering
    Update data

---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:kukeapp/models/models.dart';
+import 'package:kukeapp/shared/shared.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'custom_bottom_navbar.dart';
+part 'rating_stars.dart';
+part 'food_card.dart';
+part 'custom_tabbar.dart';
+part 'food_list_item.dart';
+part 'order_list_item.dart';
